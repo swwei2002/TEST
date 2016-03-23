29257610fb428d60c628d0d40111dc20f1ff2a27
shenwangwei <swwei2002@hotmail.com>
2016-03-23 09:43:03 +0800

    v1.1

--- gittest/Program.cs
+++ gittest/Program.cs
@@ -14,6 +14,7 @@ namespace gittest
         [STAThread]
         static void Main()
         {
+            //aaaaaa
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
